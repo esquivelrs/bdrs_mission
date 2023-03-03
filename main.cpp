33e69385951c02adad0f9253ac2c27995e92e9a0
Steven <steven.t@outlook.dk>
2023-03-03 10:54:23 +0100

    another git test..

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
  THE SOFTWARE. */
 
-/*addsda*/
+/*aiddsda*/
 
 #include <iostream>
 #include "src/ubridge.h"
